8bc5041f2b263bb762b765061d6c76f3484c497d
dwk <dwks42@gmail.com>
2019-09-09 21:22:11 -0400

    Fix warnings.

--- applet.py
+++ applet.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python2
 import signal
 import json
 
 from urllib2 import Request, urlopen, URLError
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
@@ -11,7 +16,7 @@ from gi.repository import Notify as notify
 APPINDICATOR_ID = 'wpm-x11'
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, 'sample_icon.svg', appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, gtk.STOCK_INFO, appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
