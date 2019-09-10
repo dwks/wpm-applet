e927aecba9c3cf8e1ca35bda60a393d01a33fad6
dwk <dwks42@gmail.com>
2019-09-09 22:06:20 -0400

    Successfully executing xinput and printing keypresses.

--- applet.py
+++ applet.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python2
 import signal
 import json
+import subprocess
+from threading import Thread
+import time
+
 
 from urllib2 import Request, urlopen, URLError
 
@@ -11,6 +15,7 @@ gi.require_version('Notify', '0.7')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
+from gi.repository import GObject as gobject
 
 
 APPINDICATOR_ID = 'wpm-x11'
@@ -18,6 +23,7 @@ APPINDICATOR_ID = 'wpm-x11'
 def main():
     indicator = appindicator.Indicator.new(APPINDICATOR_ID, gtk.STOCK_INFO, appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_label("text", "widest text")
     indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
     gtk.main()
@@ -46,6 +52,28 @@ def quit(_):
     notify.uninit()
     gtk.main_quit()
 
+def listen_code():
+    print("xinput pipe opening")
+    xinput = subprocess.Popen(["/usr/bin/xinput", "test", 'Lite-On Goldtouch USB Keyboard'],
+        stdout=subprocess.PIPE)
+    while True:
+        line = xinput.stdout.readline()
+        if not line: break
+        
+        col = line.split()
+        if(len(col) == 3 and col[1] == 'press'):
+            t = time.time()
+            print t, col[2]
+    print("xinput pipe closed")
+
+def listen_thread():
+    thread = Thread(target = listen_code)
+    thread.setDaemon(True)
+    thread.start()
+
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+    gobject.threads_init()
+    listen_thread()
     main()
