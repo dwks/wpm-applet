4458ab7544e5b910ed203936693e090bc82aae94
dwk <dwks42@gmail.com>
2019-09-13 22:02:13 -0400

    Finishing conversion to python classes.

--- applet.py
+++ applet.py
@@ -44,68 +44,6 @@ def main():
     appletIcon = AppletIcon()
     gtk.main()
 
-count = defaultdict(lambda : 0)
-
-def get_bucket(t):
-    return int(round(t))
-
-def update_code():
-    global count
-    start_time = get_bucket(time.time())
-    UPDATE_INTERVAL = 2
-    TIME_HORIZON = [60*60, 15*60, 5*60, 60, 10]
-    HORIZON_NAME = ["1h", "15m", "5m", "1m", "10s"]
-    max_10s = 0
-    while True:
-        time.sleep(UPDATE_INTERVAL)
-        now = get_bucket(time.time())
-        total = 0
-        horizon = len(TIME_HORIZON) - 1
-        t = 0
-        while horizon >= 0:
-            if(t == TIME_HORIZON[horizon]):
-                #window = (now - start_time + 1) if (now - t) < start_time else t
-                window = t
-                rate = total * 1.0 / window * 60
-                max_10s = max(max_10s, rate)
-                if(horizon + 1 == len(TIME_HORIZON)):
-                    print "MAX %5.1f/min," % (max_10s),
-                print "%5.1f/min @%s (%4d), " % (rate, HORIZON_NAME[horizon], total),
-                horizon -= 1
-                continue
-            c = count[now - t]
-            total += c
-            t += 1
-        print ""
-        for tt in range(1, UPDATE_INTERVAL + 10):
-            count.pop(now - t - tt, 0)  # erase element if it exists
-
-def update_thread():
-    thread = Thread(target = update_code)
-    thread.setDaemon(True)
-    thread.start()
-
-def listen_code():
-    global count
-    print("xinput pipe opening")
-    xinput = subprocess.Popen(["/usr/bin/xinput", "test", 'Lite-On Goldtouch USB Keyboard'],
-        stdout=subprocess.PIPE)
-    while True:
-        line = xinput.stdout.readline()
-        if not line: break
-
-        col = line.split()
-        if(len(col) == 3 and col[1] == 'press'):
-            t = time.time()
-            #print t, col[2]
-            count[get_bucket(t)] += 1
-    print("xinput pipe closed")
-
-def listen_thread():
-    thread = Thread(target = listen_code)
-    thread.setDaemon(True)
-    thread.start()
-
 class DaemonThread:
     def __init__(self, code, threadName = None):
         self.thread = Thread(target = code, name = threadName)
@@ -113,9 +51,67 @@ class DaemonThread:
     def start(self):
         self.thread.start()
 
+class KeyRecorder:
+    count = defaultdict(lambda : 0)
+
+    def __init__(self):
+        self.listen_thread = DaemonThread(self.listen_code, "listen-thread")
+        self.update_thread = DaemonThread(self.update_code, "update-thread")
+
+    def start(self):
+        self.listen_thread.start()
+        self.update_thread.start()
+
+    def get_bucket(self, t):
+        return int(round(t))
+
+    def update_code(self):
+        start_time = self.get_bucket(time.time())
+        UPDATE_INTERVAL = 2
+        TIME_HORIZON = [60*60, 15*60, 5*60, 60, 10]
+        HORIZON_NAME = ["1h", "15m", "5m", "1m", "10s"]
+        max_10s = 0
+        while True:
+            time.sleep(UPDATE_INTERVAL)
+            now = self.get_bucket(time.time())
+            total = 0
+            horizon = len(TIME_HORIZON) - 1
+            t = 0
+            while horizon >= 0:
+                if(t == TIME_HORIZON[horizon]):
+                    #window = (now - start_time + 1) if (now - t) < start_time else t
+                    window = t
+                    rate = total * 1.0 / window * 60
+                    max_10s = max(max_10s, rate)
+                    if(horizon + 1 == len(TIME_HORIZON)):
+                        print "MAX %5.1f/min," % (max_10s),
+                    print "%5.1f/min @%s (%4d), " % (rate, HORIZON_NAME[horizon], total),
+                    horizon -= 1
+                    continue
+                c = self.count[now - t]
+                total += c
+                t += 1
+            print ""
+            for tt in range(1, UPDATE_INTERVAL + 10):
+                self.count.pop(now - t - tt, 0)  # erase element if it exists
+
+    def listen_code(self):
+        print("xinput pipe opening")
+        xinput = subprocess.Popen(["/usr/bin/xinput", "test", 'Lite-On Goldtouch USB Keyboard'],
+            stdout=subprocess.PIPE)
+        while True:
+            line = xinput.stdout.readline()
+            if not line: break
+
+            col = line.split()
+            if(len(col) == 3 and col[1] == 'press'):
+                t = time.time()
+                #print t, col[2]
+                self.count[self.get_bucket(t)] += 1
+        print("xinput pipe closed")
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     gobject.threads_init()
-    DaemonThread(listen_code, "listen-thread").start()
-    DaemonThread(update_code, "update-thread").start()
+    KeyRecorder().start()
     main()
